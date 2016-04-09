fedb35aaedb3210118383c1747230aaef591227a
Daniel Orlovsky <dangorlov@yahoo.com>
2016-04-09 16:36:43 -0400

    Here is the initial commit

---
+++ GitHubTest/GitHubTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubTest.Startup))]
+namespace GitHubTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
